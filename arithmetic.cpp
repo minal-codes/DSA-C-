be62326a60e2d1040d4ff09b5e99c0e47a38f9a4
Minal Sharma <minalhzb@gmail.com>
2026-02-25 02:25:53 +0530

    Updated code

---
+++ arithmetic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a =5 , b= 8;
+
+    cout<< "sum = "<< (a+b)<<endl;
+    cout<< "diff = "<< (a-b)<<endl;
+    cout<< "product = "<< (a*b)<<endl;
+    cout<< "division = "<< (a/b)<<endl;
+
+    return 0;
+}
+
---
+++ logical.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << ((3>1)&&(3>4)) << endl;
+    cout << ((3>1)^(3>4)) << endl;
+    cout << (!(3>4)) << endl;
+    cout << ((3>1)||(3>4)) << endl;
+
+    return 0;
+}
+
